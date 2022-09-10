36f6490b70abbf6f9c6d3e9d3169049100b067e0
İsmet Fatih Güçlü <74713937+fatih51@users.noreply.github.com>
2022-09-10 23:11:18 +0300

    Add files via upload

---
+++ auth.guards.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { env } from "../../env";
+import { User } from "../entities/user.entity";
+import { dataSource } from "../../data-source";
+
+
+const AuthGuard = (req: express.Request, res: express.Response, next:express.NextFunction) => {
+    if (req.headers.authorization) {
+        const token = req.headers.authorization;
+        jwt.verify(token, env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401).json({
+                    message: "Invalid token"
+                });
+            } else {
+                let verified_token = jwt.decode(token) as JwtPayload;
+                if(verified_token){
+                    const userRepository = dataSource.getRepository(User);
+                    userRepository.findOneBy({ id:verified_token.id as number }).then(user=> {
+                        if (user) {
+                            req.user = user;
+                            next();
+                        } else {
+                            res.status(401).json({
+                                message: "Invalid token"
+                            });
+                        }
+                    })
+                }
+            }
+        });
+    }
+}
+
+export default AuthGuard;
